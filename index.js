66699b51344abe83be089c107957c7ed1a1ad480
ssjadhav <jadhavsaurabh156@gmail.com>
2022-04-22 21:27:32 +0530

    t

--- index.js
+++ index.js
@@ -2,33 +2,46 @@ var axios = require("axios");
 var fs = require("fs");
 var base64 = require("base-64");
 
-let token = "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";
+let token = "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";
 
 let file = fs.readFileSync("jstest.txt").toString();
 console.log(file);
 var content = base64.encode(file);
 console.log(content);
 
-// updateFile(token, content);
+updateFile(token, content);
 
 function updateFile(token, content) {
-  axios({
-    method: "put",
+  // const res = await axios.get("https://api.github.com/repos/ssjadhav156/js-test/contents/jstest.txt");
+
+ let sha = ""
+ await axios({
+    method: "get",
     url: "https://api.github.com/repos/ssjadhav156/js-test/contents/jstest.txt",
     headers: {
       Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
-    },
+    }
+  }).then(res => console.log(res))
 
-    data: JSON.stringify({
-      message: "js test",
-      content: `${content}`,
-    }),
-  })
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+
+  // await axios({
+  //   method: "put",
+  //   url: "https://api.github.com/repos/ssjadhav156/js-test/contents/jstest.txt",
+  //   headers: {
+  //     Authorization: `Bearer ${token}`,
+  //     "Content-Type": "application/json",
+  //   },
+
+  //   data: JSON.stringify({
+  //     message: "js test",
+  //     content: `${content}`,
+  //   }),
+  // })
+  //   .then(function (response) {
+  //     console.log(JSON.stringify(response.data));
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
 }
