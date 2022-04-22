416b1d152cf31c2d874e197b86268482f7381643
ssjadhav <jadhavsaurabh156@gmail.com>
2022-04-22 21:46:14 +0530

    fixed all issues

--- index.js
+++ index.js
@@ -2,46 +2,49 @@ var axios = require("axios");
 var fs = require("fs");
 var base64 = require("base-64");
 
-let token = "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";
+let token = "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";
 
-let file = fs.readFileSync("jstest.txt").toString();
-console.log(file);
-var content = base64.encode(file);
+var content = base64.encode("updating txt in file");
 console.log(content);
 
 updateFile(token, content);
 
 async function updateFile(token, content) {
-  // const res = await axios.get("https://api.github.com/repos/ssjadhav156/js-test/contents/jstest.txt");
 
- let sha = ""
- await axios({
+  let sha = "";
+  await axios({
     method: "get",
     url: "https://api.github.com/repos/ssjadhav156/js-test/contents/jstest.txt",
     headers: {
       Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
-    }
-  }).then(res => console.log(res.data))
+    },
+  })
+    .then((res) => {
+      // console.log(res.data);
+      sha = res.data.sha;
+    })
+    .catch((err) => console.log(err));
 
+  axios({
+    method: "put",
+    url: "https://api.github.com/repos/ssjadhav156/js-test/contents/jstest.txt",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
 
-  // await axios({
-  //   method: "put",
-  //   url: "https://api.github.com/repos/ssjadhav156/js-test/contents/jstest.txt",
-  //   headers: {
-  //     Authorization: `Bearer ${token}`,
-  //     "Content-Type": "application/json",
-  //   },
-
-  //   data: JSON.stringify({
-  //     message: "js test",
-  //     content: `${content}`,
-  //   }),
-  // })
-  //   .then(function (response) {
-  //     console.log(JSON.stringify(response.data));
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //   });
+    data: JSON.stringify({
+      message: "js test",
+      content: `${content}`,
+      sha: sha
+    }),
+  })
+    .then(function (response) {
+      // console.log(JSON.stringify(response.data));
+      console.log("File updated");
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 }
